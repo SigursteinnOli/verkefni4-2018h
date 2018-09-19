e0053485e3129780348234b69fd1ca434496a064
SigursteinnOli <34093550+SigursteinnOli@users.noreply.github.com>
2018-09-19 12:22:32 +0000

    Update V4.py

--- V4.py
+++ V4.py
@@ -47,7 +47,7 @@ def villa(error):
 
 
 #run(host="localhost", port =8080, reloader=True, debug=True)
-run(host="0.0.0.0", port=os.environ.get("PORT")
+run(host='0.0.0.0', port=os.environ.get('PORT')
 
 
 
